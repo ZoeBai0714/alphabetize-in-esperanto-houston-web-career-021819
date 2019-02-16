ed0044c014a90234097650d2b4d2cf2ff6945f81
Zoe Bai <s.xue.bai@gmail.com>
2019-02-16 03:02:04 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,7 @@
 # ["mi amas vin", "bonan matenon", "pacon", "ĉu vi parolas esperanton"]
 def alphabetize(arr)
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
+  arr.collect do |element|
+    
+  end
 end
